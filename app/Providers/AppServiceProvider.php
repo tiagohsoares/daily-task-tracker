17568bd00837f5d870baeb15bdb35b79f9a5a558
tiagohsoares <tiagowenceslau11@gmail.com>
2025-07-23 11:50:52 -0300

    add: Task Service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Contracts\LoginService;
 use App\Contracts\SocialiteContractService;
+use App\Models\Category;
 use Illuminate\Console\Application;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(TaskService::class, function (){
+            return $this->app->make(TaskContractService::class);
+        });
     }
     /**
      * Bootstrap any application services.
